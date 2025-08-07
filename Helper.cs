dda601e54f793de8fc34db282a43e3f18812eca0
habibaahmedabdelmalek@outlook.sa <habibaahmedabdelmalek@outlook.sa>
2025-08-07 03:00:41 +0300

    V02

--- Helper.cs
+++ Helper.cs
@@ -8,17 +8,50 @@ namespace ADV01
 {
     internal class Helper/*<T>*/
     {
+        //Generic Linear Search Method
+        public static int LinearSearch<T>(T[] Arr, T value)
+        {
+            if (Arr?.Length > 0)
+            {
+                for (int i = 0; i < Arr.Length; i++)
+                {
+                    if (Arr[i] .Equals( value)) return i;
+                }
+            }
+
+            return -1;
+        }
+
+        #region Non Generic Linear Search
+        //public static int LinearSearch(int[] Arr, int value)
+        //{
+        //    if (Arr?.Length > 0)
+        //    {
+        //        for (int i = 0; i < Arr.Length; i++)
+        //        {
+        //            if (Arr[i] == value) return i;
+        //        }
+        //    }
+
+        //    return -1;
+        //} 
+        #endregion
+
+        #region Generic Swap
         public static void Swap<T>(ref T x, ref T y)
         {
             T Temp = x;
             x = y;
             y = Temp;
-        }
+        } 
+        #endregion
 
+        #region Methods
         public static void Print<T>(T data)
         {
             Console.WriteLine(data);
-        }
+        } 
+        #endregion
 
         #region Non Generic
         //public static void Swap(ref int x , ref int y)
--- Program.cs
+++ Program.cs
@@ -90,9 +90,26 @@
             //Console.WriteLine($"P02 : {P02}"); 
             #endregion
 
+            //Linear Search 
 
+            // 4 7 8 9 10 1 2 3 5 12 -1 -13 11
 
+            //Index 
 
+            //int[] Numbers = { 8, 7, 6, 1, 2, 3, 4, 5, 6, 12, -1, 13, 9 };
+
+            //int Index = Helper.LinearSearch(Numbers, 4);
+
+            //Console.WriteLine($"Index : {Index}");
+
+            //Employee E01 = new Employee() { Id = 1, Name = "Ahmed", Age = 31, Salary=12000 };
+            //Employee E02 = new Employee() { Id = 2, Name = "Ali", Age = 34, Salary=11000 };
+            //Employee E03 = new Employee() { Id = 3, Name = "Omar", Age = 32, Salary=13000 };
+            //Employee E04 = new Employee() { Id = 4, Name = "Mona", Age = 39, Salary=19000 };
+
+            //Employee[] employee = { E01, E02, E03, E04, };
+
+            //Helper.LinearSearch(employee, E01);
         }
     }
 }
