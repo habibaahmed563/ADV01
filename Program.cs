027773ba9b2858f2b0af156195fb5d0d3c9b28c6
habibaahmedabdelmalek@outlook.sa <habibaahmedabdelmalek@outlook.sa>
2025-08-09 23:00:58 +0300

    Q1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //Q1
+
+            var r1 = new Range<int>(2, 10);
+            Console.WriteLine(r1);
+            Console.WriteLine(r1.IsRange(3));
+            Console.WriteLine(r1.IsRange(16));
+
+            var r2 = new Range<double>(2.5, 10.5);
+            Console.WriteLine(r2.Length);
+
+            var r3 = new Range<int>(12, 4);
+            Console.WriteLine(r3);
+
+
+            
+
         }
     }
 }
