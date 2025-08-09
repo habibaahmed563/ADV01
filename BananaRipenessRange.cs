66269878505acf0c24bb4e57713616df95bc1910
habibaahmedabdelmalek@outlook.sa <habibaahmedabdelmalek@outlook.sa>
2025-08-10 00:26:12 +0300

    Task01

---
+++ BananaRipenessRange.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADV01
+{
+    internal class BananaRipenessRange<T> where T : IComparable<T>
+    {
+        public T minRipeness;
+        public T maxRipeness;
+
+        public BananaRipenessRange(T min,T max)
+        {
+            minRipeness = min;
+            maxRipeness = max;
+        }
+
+        public bool Isperfectbanana(T ripeness)
+        {
+            return ripeness.CompareTo(minRipeness) >= 0 && ripeness.CompareTo(maxRipeness) <= 0;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            BananaRipenessRange<int> bananaRange = new BananaRipenessRange<int>(5, 10);
+
+            Console.WriteLine(bananaRange.Isperfectbanana(7));
+            Console.WriteLine(bananaRange.Isperfectbanana(2));
+
+            BananaRipenessRange<double> bananaRangedouble = new BananaRipenessRange<double>(5.5, 10.6);
+
+            Console.WriteLine(bananaRangedouble.Isperfectbanana(4.8));
+            Console.WriteLine(bananaRangedouble.Isperfectbanana(8.3));
+
         }
     }
 }
